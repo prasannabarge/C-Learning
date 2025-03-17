3848c876bafa61de8467332e1b675d99c35b7426
prasannabarge <prasannabarge08@gmail.com>
2025-03-17 22:26:20 +0530

    C Input Output Functions

---
+++ 21.C Input Ouput Functions/2.Unformatted Input and output/O/01.getch.c
@@ -0,0 +1,14 @@
+//C Program to implement getch()function
+
+#include <conio.h>
+#include <stdio.h>
+
+//Driver code
+int main(void){
+  printf("Enter any character: ");
+
+  //Reads a character but not displays
+  getch();
+
+  return 0 ;
+}
