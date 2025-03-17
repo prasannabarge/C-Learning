a4d41464aea0b634b01a42373461dff3fb06fb55
prasannabarge <prasannabarge08@gmail.com>
2025-03-17 22:40:24 +0530

    C Input Output Functions

---
+++ 21.C Input Ouput Functions/2.Unformatted Input and output/02.getche.c
@@ -0,0 +1,15 @@
+//C Program to implement  the getche() function
+
+#include <conio.h>
+#include <stdio.h>
+
+//Driver code
+int main(void){
+
+  printf("Enter any character: ");
+
+  //Reads a character and displays immediately
+  getche();
+
+  return 0 ;
+}
