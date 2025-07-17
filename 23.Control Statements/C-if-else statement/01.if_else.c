e5d5a025b313eb389d5707fafe5c9fd7ddbeee74
prasannabarge <prasannabarge08@gmail.com>
2025-07-17 20:40:41 +0530

    C if statement

---
+++ 23.Control Statements/C-if-else statement/01.if_else.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+   
+  int n  = 10 ;
+
+  if(n>5){
+    printf("%d is greater than 5",n);
+  }
+
+  else{
+    printf("%d is less than 5",n);
+
+
+  }
+
+  return 0;
+
+}
+
+/* 
+ if(condition){
+   / code to execute if condition is true
+ }
+   else{
+   /code to execute if condition is false
+   }
+   
+*/
---
+++ 23.Control Statements/C-if-else statement/02.if_else_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+
+   int n  = -7;
+
+   if(n<0){
+    printf("the no is negative");
+   }
+   else{
+    printf("the no is not negative");
+   }
+
+   return 0;
+   
+}
